aa342c4a98f44710dcdd253d14201ad51f1a0d58
Sai <helen_gemini@126.com>
2018-01-16 23:10:39 +0800

    Finish inputbox

--- js/InputBox.js
+++ js/InputBox.js
@@ -5,5 +5,8 @@ InputBox.prototype.render = function(){
  var wrapper = document.createElement("div");
  wrapper.appendChild(document.createElement("input"));
 wrapper.children[0].className = "inputBox";
+wrapper.children[0].placeholder = "What do you want to do?";
+wrapper.appendChild(document.createElement("div"));
+wrapper.children[1].className = "fa fa-plus forPlus";
 return wrapper;
 };
