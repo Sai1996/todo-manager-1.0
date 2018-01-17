d42151f6c12ed4d896a0ae7128c986fb68d88eaa
Sai <helen_gemini@126.com>
2018-01-18 00:10:44 +0800

    Fix the position problem & finish the checkbox style by copying from codePen.

--- js/InputBox.js
+++ js/InputBox.js
@@ -3,10 +3,11 @@ function InputBox(){
 };
 InputBox.prototype.render = function(){
  var wrapper = document.createElement("div");
+ wrapper.className = "forInputboxPos";
  wrapper.appendChild(document.createElement("input"));
 wrapper.children[0].className = "inputBox";
 wrapper.children[0].placeholder = "What do you want to do?";
 wrapper.appendChild(document.createElement("div"));
-wrapper.children[1].className = "fa fa-plus forPlus";
+wrapper.children[1].className = "fa fa-plus plus";
 return wrapper;
 };
--- js/TodoItem.js
+++ js/TodoItem.js
@@ -1,11 +1,21 @@
-function TodoItem(){
-  this.textContent = "";
-  this.isEditting = false;
-  this.isCompleted = false;
-  this.html = "";
+function TodoItem(text,isEditting,isCompleted,isDelete){
+  this.textContent = text;
+  this.isEditting = isEditting;
+  this.isCompleted = isCompleted;
+  this.delete = isDelete;
 }
 TodoItem.prototype.render = function(){
   var wrapper = document.createElement("div");
+  wrapper.appendChild(document.createElement("div"));
+  wrapper.children[0].appendChild(document.createElement("div"));
+  wrapper.children[0].children[0].className = "squaredFour";
+  wrapper.children[0].children[0].appendChild(document.createElement("input"));
+  wrapper.children[0].children[0].children[0].type = "checkbox";
+  wrapper.children[0].children[0].children[0].value = "None";
+  wrapper.children[0].children[0].children[0].id = "squaredFour";
+  wrapper.children[0].children[0].children[0].name = "check";
+  wrapper.children[0].children[0].appendChild(document.createElement("label"));
+  wrapper.children[0].children[0].children[1].setAttribute('for', "squaredFour");
   return wrapper;
 };
 
